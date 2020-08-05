61aad7767a1f0a17256b6d58f13cac9a694bf9c3
supercoder95 <supercoder@kakao.com>
2020-08-06 01:51:22 +0900

    super

--- begin-app/src/useInputs.js
+++ begin-app/src/useInputs.js
@@ -1,11 +1,15 @@
 import { useState, useCallback } from 'react';
 
 function useInputs(initialFrom) {
-    const [from, setForm] = useState(initialFrom);
+    const [form, setForm] = useState(initialFrom);
     const onChange = useCallback((e) => {
         const { name, value } = e.target;
         setForm(form => ({ ...form, [name]: value }));
     }, [])
 
     const reset = useCallback(() => setForm(initialFrom), [initialFrom]);
+
+    return [form, onChange, reset];
 };
+
+export defalut useInputs;
