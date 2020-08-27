8151a273e795375c215874404f26aed91a4f80ff
supercoder <kazehazaya@gmail.com>
2020-08-27 19:05:10 +0900

    asdfasdfadsf

--- styling-with-styled-components/src/App.js
+++ styling-with-styled-components/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import sytled from 'styled-components';
+import styled from 'styled-components';
+import Button from './components/Button'
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
 
 function App() {
-  return <div />;
+  return (
+    <AppBlock>
+      <Button>BUTTON</Button>
+    </AppBlock>
+  );
 }
 
 export default App;
