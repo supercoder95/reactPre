a71e51e6936dbbffcec1ea4b47b834ad4ba419e0
supercoder <kazehazaya@gmail.com>
2020-09-08 11:22:14 +0900

    supercoder

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function Users() {
-    return (
-        <div>
+    const [users, setUsers] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-        </div>
-    )
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                setUsers(null);
+                setError(null);
+                setLoading(true);
+                const response = await axios.get(
+                    'https://jsonplaceholder.typicode.com/users/'
+                )
+            } catch (e) {
+
+            }
+        }
+    }, [])
+
+    return <div />
 }
 
 export default Users
