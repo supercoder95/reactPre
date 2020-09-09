524b639839643d0b8d0005e5ff5016eab0f525b7
supercoder95 <supercoder@kakao.com>
2020-09-10 00:59:34 +0900

    supercoder

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -6,22 +6,42 @@ function Users() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                setUsers(null);
-                setError(null);
-                setLoading(true);
-                const response = await axios.get(
-                    'https://jsonplaceholder.typicode.com/users/'
-                )
-            } catch (e) {
-
-            }
+    const fetchUsers = async () => {
+        try {
+            setUsers(null);
+            setError(null);
+            setLoading(true);
+            const response = await axios.get(
+                'https://jsonplaceholder.typicode.com/users/'
+            );
+            setUsers(response.data);
+        } catch (e) {
+            setError(e);
         }
-    }, [])
+        setLoading(false);
+    }
+    useEffect(() => {
 
-    return <div />
+        fetchUsers();
+    }, []);
+
+    if (loading) return <div>로딩중...</div>
+    if (error) return <div>에러가 발생하였습니다.</div>
+    if (!users) return null;
+
+
+    return (
+        <>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>
+                        {user.username} ({user.name})
+                    </li>
+                ))}
+            </ul>
+            <button onClick={fetchUsers}>다시 불러오기</button>
+        </>
+    );
 }
 
 export default Users
